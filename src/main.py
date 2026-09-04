8cab74763bc8b17f1b5f93960987fbf20dc8e5a5
chadd williams <will4614@pacificu.edu>
2026-09-04 11:04:19 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
-################################################################################
-# File name:    main.py
-# Author:       YOUR NAME HERE
-# Date:         5/14/2025
-# Class:        CS 170
-# Assignment:   01Lab
-# Purpose:      Demonstrate Codespaces & Python
-# Hours:        1.5
-################################################################################
+"""Example coding standards
+File name:    main.py
+Author:       YOUR NAME HERE
+Date:         5/14/2025
+Class:        CS 300
+Assignment:   01Lab
+Purpose:      Demonstrate coding standards
+Hours:        1.5
+
+"""
 
 from datetime import date
 
-print('Hello world!')
-
-print('Today is', date.today())
+if __name__ == '__main__':
+  
+  print('Hello world!')
+  
+  print('Today is', date.today())
